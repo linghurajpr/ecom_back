8c5d6dc8545c9b8de706954dee8cc7ecc8b7b0d5
linghurajpr <linghurajpr>
2025-07-16 15:40:43 +0530

    admin product regiter,cart,cartitems

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Product ,Cart,CartItems
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['id','name','category','price','in_stock']
+    search_fields=['name','category']
+    list_display=['category','instock']
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'price', 'in_stock']
+    search_fields = ['name', 'category']
+    list_filter = ['category', 'in_stock']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created']
+    date_hierarchy = 'created'
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cart', 'product', 'quantity']
+    list_filter = ['product']
